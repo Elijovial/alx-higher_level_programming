072408757d8aab74338178241d2f2fbbac4ffa7c
Elijovial <omeehelijah345@gmail.com>
2023-08-10 21:50:55 -0700

    Task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            #subtract 32 to get the corresponding uppercase
+            c = chr(ord(c) - 32)
+            print("{}".format(c), end="")
+            print("\n")
