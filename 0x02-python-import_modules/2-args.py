047644592812b9c9a379ce8d9434daf0d02b13e7
Elijovial <omeehelijah345@gmail.com>
2023-08-12 13:50:37 -0700

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def my_function(argv):
         if len(argv) == 1:
-            print("{:d} argument.".format(len(argv) - 1))
+            print("{:d} arguments.".format(len(argv) - 1))
         elif len(argv) == 2:
             print("{:d} argument:".format(len(argv) - 1))
             print("{:d}: {}".format(1, argv[1]))
