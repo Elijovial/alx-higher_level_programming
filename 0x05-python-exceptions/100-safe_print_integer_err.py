fb650d457a11c2d2b22cf86ee4801d43a258c522
Elijovial <omeehelijah345@gmail.com>
2023-08-30 09:15:57 -0700

    Task 100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+import sys
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    try:
+        sys.stdout.write("{:d}\n".format(value))
+        return True
+    except Exception as e:
+        sys.stderr.write("Exception: {}\n".format(e))
+        return False
