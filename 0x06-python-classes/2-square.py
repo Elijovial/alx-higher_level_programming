ff31054b092d8cdc0e3b915fbda0af7f5a0660f0
Elijovial <omeehelijah345@gmail.com>
2023-08-30 20:56:49 -0700

    Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module contains a class Square that defines a square."""
+
+
+class Square:
+    """A class Square that defines a square."""
+    def __init__(self, size=0):
+        """Initializes an instance of the Square class."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
