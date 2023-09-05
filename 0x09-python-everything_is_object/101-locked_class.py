61f575ce4d5b86dda4099646c585710ddb69f6fd
Elijovial <omeehelijah345@gmail.com>
2023-09-05 13:03:21 -0700

    Task 110

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+class LockedClass:
+    """A class that prevents the user from dynamically creating new instance attributes, except if the new instance attribute is called first_name."""
+
+    __slots__ = ["first_name"] # define a list of allowed attributes using __slots__
+
+    def __init__(self):
+        pass # no need to initialize anything
