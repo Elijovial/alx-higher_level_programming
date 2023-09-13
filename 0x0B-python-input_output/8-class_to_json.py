668802d4346624903fadae9d3e08ada6badbf4b3
Elijovial <omeehelijah345@gmail.com>
2023-09-13 07:08:31 -0700

    Task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Write a function"""
+
+
 def class_to_json(obj):
     """returns the dictionary description
     with simple data structure for JSON serialization of an object"""
