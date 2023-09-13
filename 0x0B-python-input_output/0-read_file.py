1571ea7d42e0428d76cb479570efd5b3ff7e1a0d
Elijovial <omeehelijah345@gmail.com>
2023-09-12 19:49:31 -0700

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Reads a text file (UTF8) and prints it to stdout"""
+"""Reads a text file (UTF-8)"""
 
 
 def read_file(filename=""):
+    """Print the contents of the file to the stdout"""
     with open(filename, encoding="utf-8") as f:
         for line in f:
             print(line, end="")
