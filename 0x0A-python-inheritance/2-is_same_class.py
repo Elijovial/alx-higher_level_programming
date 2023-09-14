38f1f98124d0182bce16faea57bf022845e8832e
Elijovial <omeehelijah345@gmail.com>
2023-09-13 21:13:03 -0700

    Task 1

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Write a function"""
+
+
+def is_same_class(obj, a_class):
+    """A function that checks if an object is exactly an instance of a class"""
+
+    return type(obj) is a_class
