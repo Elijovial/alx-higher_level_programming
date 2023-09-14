5a74735b3c52b4966da999b35a16c0e42c109c1c
Elijovial <omeehelijah345@gmail.com>
2023-09-13 21:23:35 -0700

    Task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write a function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """A function that checks if an object
+    is an instance of a class or a subclass"""
+
+    return isinstance(obj, a_class)
