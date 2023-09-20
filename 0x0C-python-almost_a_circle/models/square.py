ff9cbd1f20abc67bc568cc3b309bb9d5348e5cc8
Elijovial <omeehelijah345@gmail.com>
2023-09-20 09:57:29 -0700

    Task 14

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -72,3 +72,16 @@ key/value arguments to assign to the attributes.
             for key, value in kwargs.items():
                 # Use setattr to assign the value to the attribute name
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Return the dictionary representation of a Square."""
+
+        # Create a new dictionary with the attributes of the instance
+        dictionary = {}
+        # Use a loop to iterate over the attribute names
+        for attribute in ["id", "size", "x", "y"]:
+            # Use getattr to get the value
+            # of each attribute and assign it to the dictionary
+            dictionary[attribute] = getattr(self, attribute)
+        # Return the dictionary
+        return dictionary
